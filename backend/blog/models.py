f86a5958a8fda6790103d296f3086dc73e308f5d
OsbornDelAngel <ozdelangel@gmail.com>
2022-06-19 01:46:25 -0700

    initial setup and models

---
+++ backend/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Posts(models.Model):
+    name = models.CharField(max_length=64)
+    date = models.DateTimeField(auto_now=True)
+    post = models.TextField(default='')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
